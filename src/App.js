b60f560e17880083d8b8046996af63d359fd3490
Angelpantoja1 <Jangelpantoja27@gmail.com>
2022-05-06 14:30:54 -0700

    Material UI DataGrid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
+import { useState } from 'react';
+import { Button } from '@mui/material';
 
 function App() {
+  const [test, setTest] = useState([]);
+
+  const rows = [
+    { id: 1, col1: 'Hello', col2: 'World' },
+    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+    { id: 3, col1: 'MUI', col2: 'is Amazing' },
+  ];
+  const columns = [
+    { field: 'col1', headerName: 'Column 1', width: 150 },
+    { field: 'col2', headerName: 'Column 2', width: 150 },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: 300, width: '100%' }}>
+      <DataGrid rows={rows} columns={columns} checkboxSelection onSelectionModelChange={ (e) => setTest(e)}/>
+      {test.toString()}
+      <Button disabled={test.length ===0 ? true: false} >Click</Button>
     </div>
   );
 }
